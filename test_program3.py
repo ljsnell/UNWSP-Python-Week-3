3209f5d1fb9deb4d4d7cc4b8b18b9236a1298e56
ljsnell <ljsnell@users.noreply.github.com>
2025-09-20 18:50:46 -0500

    Formatting and test fixing. (#2)

--- test_program3.py
+++ test_program3.py
@@ -2,13 +2,13 @@ import program_3
 
 # Using string conversions in case they elect to not add trailing 0's in their logic.
 def test_11_weight():
-    assert "4.75" in str(program_3.weight_conversion(11))
+    assert "52.25" in str(program_3.weight_conversion(11))
 
 def test_7_weight():
-    assert "4" in str(program_3.weight_conversion(7))
+    assert "28" in str(program_3.weight_conversion(7))
 
 def test_3_weight():
-    assert "3" in str(program_3.weight_conversion(3))
+    assert "9" in str(program_3.weight_conversion(3))
 
 def test_1_weight():
     assert "1.5" in str(program_3.weight_conversion(1))
